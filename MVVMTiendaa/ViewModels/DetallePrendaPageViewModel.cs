6c6c8d81070011cc928016ea8c62b8d539cc0c23
Kriss Cerón <krissceron03@outlook.com>
2024-01-10 00:31:42 -0500

    Ya está DetallePrendaPage con MVVM

---
+++ MVVMTiendaa/ViewModels/DetallePrendaPageViewModel.cs
@@ -0,0 +1,118 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls;
+using MVVMTiendaa.Models;
+using MVVMTiendaa.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTiendaa.ViewModels
+{
+    public partial class DetallePrendaPageViewModel: ObservableObject  
+    {
+        private APIService _ApiService;
+        [ObservableProperty]
+        public Prenda prenda;
+        [ObservableProperty]
+        public string nombre;
+        [ObservableProperty]
+        public string cantidad;
+        [ObservableProperty]
+        public int cantidadPicker;
+        [ObservableProperty]
+        public string descripcion;
+        [ObservableProperty]
+        public string categoria;
+        [ObservableProperty]
+        public string categoriaDescripcion;
+        [ObservableProperty]
+        public string marca;
+        [ObservableProperty]
+        public string marcaDescripcion;
+        [ObservableProperty]
+        public List<string> num;
+        [ObservableProperty]
+        public int cantidadMaximaProductos;
+
+
+        public DetallePrendaPageViewModel()
+        {
+            
+        }
+
+        public void SetAPIService(APIService apiService)
+        {
+            _ApiService = apiService;
+        }
+
+        [RelayCommand]
+        public async Task<int> OnClickAnadir()
+        {
+
+            int usuarioid = Preferences.Get("idUsuario", 0);
+            if (usuarioid == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                //toma el indice del picker y suma uno para saber la cantidad que mando el usuario
+                var cantidadSeleccionada = CantidadPicker;
+                if (cantidadSeleccionada == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    cantidadSeleccionada = CantidadPicker + 1;
+
+                    int carritoid = Preferences.Get("idCarrito", 0);
+                    DetalleCarritoUsuario detalleCarritoUsuario = new DetalleCarritoUsuario
+                    {
+                        cantidad = cantidadSeleccionada,
+                        prendaIdPrenda = Prenda.idPrenda,
+                        carritoIdCarrito = carritoid,
+                    };
+                    DetalleCarrito detallecarrito = await _ApiService.PostDetalleCarrito(detalleCarritoUsuario);
+                    return 1;
+                }
+              
+            }
+        }
+        public async Task CargarPrendasAsync(int idPrenda)
+        {
+            Prenda = await _ApiService.GetPrenda(idPrenda);
+            //actualización
+
+            Nombre = Prenda.nombre;
+            Cantidad = Prenda.cantidad.ToString();
+            CantidadMaximaProductos = Prenda.cantidad;
+            Descripcion = Prenda.descripcion;
+            Marca = Prenda.marca.nombre;
+            Categoria = Prenda.categoria.nombre;
+            MarcaDescripcion = Prenda.marca.descripcion;
+            CategoriaDescripcion = Prenda.categoria.descripcion;
+            
+        }
+
+        public async Task<List<string>> CargarStock(int idPrenda)
+        {
+            Prenda = await _ApiService.GetPrenda(idPrenda);
+            CantidadMaximaProductos = Prenda.cantidad;
+            // cargamos la lista de numeros
+            List<string> num = new List<string>();
+
+                for (int i = 1; i <= CantidadMaximaProductos; i++)
+                {
+                    num.Add(i.ToString());
+                }
+
+                // asignamos al item source de la vista
+                return num;
+            
+        }
+    }
+}
--- MVVMTiendaa/Views/DetallePrendaPage.xaml.cs
+++ MVVMTiendaa/Views/DetallePrendaPage.xaml.cs
@@ -1,20 +1,26 @@
 using MVVMTiendaa.Models;
 using MVVMTiendaa.Services;
+using MVVMTiendaa.ViewModels;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MVVMTiendaa;
 
 public partial class DetallePrendaPage : ContentPage
 {
-    private Prenda _prenda;
+ 
     private readonly APIService _ApiService;
-    public List<string> num { get; set; }
-    private int cantidadMaximaProductos;
+    private readonly int _idPrenda;
+    private readonly DetallePrendaPageViewModel _viewModel;
 
-    public DetallePrendaPage(APIService apiservice, int idprenda)
+
+    public DetallePrendaPage(APIService apiservice, int idPrenda)
     {
         InitializeComponent();
         _ApiService = apiservice;
+        _viewModel = new DetallePrendaPageViewModel();
+        _viewModel.SetAPIService(apiservice);
+        _idPrenda = idPrenda;
+        BindingContext = _viewModel;
 
         int usuarioid = Preferences.Get("idUsuario", 0);
         if (usuarioid == 0)
@@ -23,20 +29,20 @@ public partial class DetallePrendaPage : ContentPage
             Preferences.Set("idCarrito", 0);
 
         }
+
     }
-    protected override void OnAppearing()
+
+
+
+    
+
+    protected async override void OnAppearing()
     {
-        base.OnAppearing();
-        _prenda = BindingContext as Prenda;
-        Nombre.Text = _prenda.nombre;
-        Cantidad.Text = _prenda.cantidad.ToString();
-        cantidadMaximaProductos = _prenda.cantidad;
-        Descripcion.Text = _prenda.descripcion;
-        Marca.Text = _prenda.marca.nombre;
-        Categoria.Text = _prenda.categoria.nombre;
-        MarcaDescripcion.Text = _prenda.marca.descripcion;
-        CategoriaDescripcion.Text = _prenda.categoria.descripcion;
-        CargarStock();
+      base.OnAppearing();
+        _viewModel.CargarPrendasAsync(_idPrenda);
+        List<string> num = await _viewModel.CargarStock(_idPrenda);
+        cantidad.ItemsSource= num;
+        
     }
 
     private async void OnClickSalir(object sender, EventArgs e)
@@ -51,53 +57,28 @@ public partial class DetallePrendaPage : ContentPage
 
     private async void OnClickAnadir(object sender, EventArgs e)
     {
-       
-        int usuarioid = Preferences.Get("idUsuario", 0);
-        if (usuarioid == 0)
+        int resultado = await _viewModel.OnClickAnadir();
+        if(resultado == -1)
         {
             await Navigation.PushAsync(new LoginPage(_ApiService));
         }
-        else 
+        else
         {
-            //toma el indice del picker y suma uno para saber la cantidad que mando el usuario
-            var cantidadSeleccionada = cantidad.SelectedIndex;
-            var cantidad1 = cantidadSeleccionada + 1;
-
-            int carritoid = Preferences.Get("idCarrito", 0);
-
-
-
-            DetalleCarritoUsuario detalleCarritoUsuario = new DetalleCarritoUsuario
+            if(resultado == 0)
             {
-                cantidad = cantidad1,
-                prendaIdPrenda = _prenda.idPrenda,
-                carritoIdCarrito = carritoid,
-            };
-
-            DetalleCarrito detallecarrito = await _ApiService.PostDetalleCarrito(detalleCarritoUsuario);
-            await DisplayAlert("Exito!", "Agregaste el producto a tu carrito con éxito", "OK");
-            await Navigation.PopAsync();
+                await DisplayAlert("NO!", "AGREGA ALGO AL CARRITO", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Exito!", "Agregaste el producto a tu carrito con éxito", "OK");
+                await Navigation.PopAsync();
+            }
+        }
+       
+            
 
             //var toast = CommunityToolkit.Maui.Alerts.Toast.Make("Usuario o contraseña incorrecta", ToastDuration.Short, 14);
 
             //await toast.Show();
         }
     }
-
-
-
-    private void CargarStock()
-    {
-        // cargamos la lista de numeros
-        num = new List<string>();
-        for (int i = 1; i <= cantidadMaximaProductos; i++)
-        {
-            num.Add(i.ToString());
-        }
-
-        // asignamos al item source de la vista
-        cantidad.ItemsSource = num;
-    }
-
-
-}
